45df1a8e64f91e71480bafafa5c12be21ca38e5e
nfrederick1 <nfrederick1@pride.hofstra.edu>
2021-03-09 20:49:53 +0000

    added output files

--- generate_random.cpp
+++ generate_random.cpp
@@ -30,26 +30,26 @@ int main()
     for (int i = 0; i < 10; i ++)
         cout << rand() << '\t';
     cout << "\nQ1: What is the output of rand(). Why? " << endl;
-    answer = "" ; // 1. Write your answer to Q1 in between " "
+    answer = "The output of rand() 10 numbers between 0 and RANDMAX" ; // 1. Write your answer to Q1 in between " "
     cout << "Answer Q1: " << answer << endl;
 
     cout << endl;
     for (int i = 0; i < 10; i ++)
         cout << rand()%5 << '\t';
     cout << "\nQ2: What is the output of rand()%5. Why? " << endl;
-    answer = "" ; // 2. Write your answer to Q2 in between " "
+    answer = "The output of rand()%5 is 10 numbers between 0 and 4 (inclusive)" ; // 2. Write your answer to Q2 in between " "
     cout << "Answer Q2: " << answer << endl;
 
     cout << endl;
     for (int i = 0; i < 10; i ++)
         cout << rand()%5+3 << '\t';
     cout << "\nQ3: What is the output of rand()%5+3. Why?" << endl;
-    answer = "" ; // 3. Write your answer to Q3 in between " "
+    answer = "The output of rand()%5+3 is 10 numbers between 0 and 4 and then adding 3 to them" ; // 3. Write your answer to Q3 in between " "
     cout << "Answer Q3: " << answer << endl;
 
     // Comment the line srand(time(NULL)); compile and run the program twice
     cout << "\nQ4: What is the output of the program when srand(time(NULL)) is removed. Why" << endl;
-    answer = "" ; // 4. Write your answer to Q4 in between " "
+    answer = "The code still operated but ran the same numbers It continued because the srand function allows for new random generations." ; // 4. Write your answer to Q4 in between " "
     cout << "Answer Q4: " << answer << endl;
 
     // 5. Change the rand() expression below to generate a random number 
@@ -58,10 +58,11 @@ int main()
     bool is_error = false; 
     for (int i = 0; i < 50; i ++)
     {
-        int dice_output = rand(); //change this expression so dice_output has a value between 1 and 6
+        int dice_output = rand()%6+1; //change this expression so dice_output has a value between 1 and 6
         if (dice_output < 1 || dice_output > 6)
         {
             cout << "Error: your code does not generate # between 1 and 6" << endl;
+            cout << dice_output << endl; 
             is_error = true;
             break;  // exits the loop
         }
--- play_dice.cpp
+++ play_dice.cpp
@@ -8,12 +8,64 @@ using namespace std;
 
 int main()
 {
-    srand(time(0)); // initialize random seed generator to current time: 
+    srand(time(NULL)); // initialize random seed generator to current time: 
 					// needed to generate a different sequence of random numbers
 					// each time you run your program
+    int die_1; // dice
+    int die_2; // dice
+    int dice_sum; // the sum of the dice
+    int point = 0; // The holder point 
+    bool final_roll= false; //  terminates loop if anything happens to end the game
+    char decision; // Decides if the game will be played again 
+    cout << "Welcome to a game of Craps: The game of craps is perhaps the most famous of all dice games. The player begin by throwing two standard dice.\nEach dice can have a value of 1 to 6. If the sum of these dice is 7 or 11, the player wins.\nIf the sum is 2,3 or 12, the player loses. Otherwise, the sum becomes the player’s point.\nThe player continues to roll until either the point comes up again, in which case the player wins, or the player throws 7, in which case they lose. ";
+    cout << "Press Y to start and N to exit. ";
+    cin >> decision;
+    while (decision == 'y' || decision == 'Y') //Checks the decision
+    {
+        die_1 = rand() % 6 +1;
+        die_2 = rand() % 6 +1;
+        dice_sum = die_1 + die_2;
+        cout << "You rolled a " << die_1 << " and a " << die_2 << ". Your sum is " << dice_sum << endl;
+        point += dice_sum;
+        while (final_roll == false)
+        {
+           if (dice_sum == 7 || dice_sum == 11)
+           {
+               cout << "Your sum was " << dice_sum << " Congraulations You Won" << endl; break;
+               final_roll = true;
 
-    // Add your code to play the dice game here
-
+           }else if(dice_sum == 2 || dice_sum == 3 || dice_sum == 12)
+           {
+              cout << "Your sum was " << dice_sum << " Sorry but you lost" << endl; break;
+              final_roll = true;
+           }else{
+              point += dice_sum;
+              cout << "Your point is " << point << " Let's roll Again" << endl; break;
+              final_roll = true; 
+              while (point != dice_sum)
+              {
+                if ((dice_sum == 7) || dice_sum == 2 || dice_sum == 3 || dice_sum == 12){
+                    cout << "Sorry but you rolled a " << dice_sum << " and lost" << endl;break;
+                }
+                else if(point == dice_sum)
+                {
+                    cout << "Your sum was " << dice_sum << " Congraulations You Won" << endl; break;
+                }
+                else
+                    break;
+                
+              }
+            }
+        }
+    }
+           
+        
+        
+       cout << "Do you want to roll again? (Y/N) ";
+       cin >> decision; 
+    cout << "Thanks for playing." << endl;
 }
+
+
 
 
